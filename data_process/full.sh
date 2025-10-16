c536b796ee6bb636cba322bc88cf71373e72a9b2
Zhodong Liu <zl4789@nyu.edu>
2025-10-16 07:54:00 -0400

    update

--- data_process/full.sh
+++ data_process/full.sh
@@ -5,7 +5,7 @@
 #SBATCH --cpus-per-task=10
 #SBATCH --mem=64GB
 #SBATCH --account=pr_119_tandon_priority
-#SBATCH --time=10:00:00
+#SBATCH --time=3:00:00
 #SBATCH --gres=gpu:1
 #SBATCH --mail-type=ALL          
 #SBATCH --mail-user=zl4789@nyu.edu
@@ -16,10 +16,10 @@ conda activate multimodal
 cd /scratch/zl4789/MQL4GRec
 
 
-bash /scratch/zl4789/MQL4GRec/data_process/0_download.sh
-bash /scratch/zl4789/MQL4GRec/data_process/1_load_figure.sh
-bash /scratch/zl4789/MQL4GRec/data_process/2_process.sh
-# bash /scratch/zl4789/MQL4GRec/data_process/3_get_text_emb.sh
-bash /scratch/zl4789/MQL4GRec/data_process/4_get_image_emb.sh
+# bash /scratch/zl4789/MQL4GRec/data_process/0_download.sh
+# bash /scratch/zl4789/MQL4GRec/data_process/1_load_figure.sh
+# bash /scratch/zl4789/MQL4GRec/data_process/2_process.sh
+bash /scratch/zl4789/MQL4GRec/data_process/3_get_text_emb.sh
+# bash /scratch/zl4789/MQL4GRec/data_process/4_get_image_emb.sh
 
 conda deactivate
